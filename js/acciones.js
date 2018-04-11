e56e3a21239097c4a062a7d75f8cac2cc6ea1e1b
AnthonyMarGJInfinity117 <marvingj17>
2018-04-11 13:26:19 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
-// JavaScript Document
+ // JavaScript Document
 
  $(document).ready(function (e){
-	document .addEventListener("deviceready", function(){
+	document .addEventListener("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordova);
 		$('#disp table td').eq(7).text(device.platform);
@@ -14,11 +14,11 @@
 		escribehistoria ('la app se reinició');
 	},false);//resume
 	document.addEventListener("online",function(){
-		escribehistoria ('la app se conecto a la red');
+		escribehistoria ('la app se conectó a la red');
 	},false);//se conecto
 	document.addEventListener("offline",function(){
-		escribehistoria ('la app se desconecto de la red');
-	},false);//pause
+		escribehistoria ('la app se desconectó de la red');
+	},false);//se desconecto
 	
 	
 	},false);//ready device
@@ -27,5 +27,5 @@
  });//document
  
  function escribehistoria(accion){
-	 $('eHistoria').append('<li>'+accion+'<li>');
+	 $('#eHistoria').append('<li>'+accion+'</li>');
  }
